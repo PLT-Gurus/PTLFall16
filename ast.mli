b6131b160865c2018446897f7f5711e0d900cdbf
Min Fan <Min@dyn-160-39-246-123.dyn.columbia.edu>
2016-10-29 23:21:30 -0400

    AST file is completed.

--- ast.mli
+++ ast.mli
@@ -1,19 +1,38 @@
-type uop = ASSIGN | 
+type uop = Expon | Neg | Comp | Transcb | Translt | Translttwo
 
-type op = Add | Sub | Mul | Div |
+type op = Add | Sub | Mult | Div | Mod | And | Or | Not | Equal | Neq | Less | Leq | Greater | Geq
 
-type typ =
-	Litint of int
-|	Sequence of string
+type typ = Int | Bool | Void | Char | Double | Aa | Nuc | Codon | Seq
 
-type typename = Int | Seq
-type bind = typename * string
-type expr =
-	Litint of int
-|	Sequence of string
-| 	Binop of expr * op * expr
-type stmt =
-	VDecl of typename * string * expr (* replace with bind *)
-|   Semi
-|   Binop of expr * op * expr
-type program = stmt
+type ending = End
+
+type bind = typ * string
+
+type expr = Litint of int
+		| Id of string
+		| Sequence of string
+		| Litbool of bool
+		| Noexpr
+		| Binop of expr * op * expr
+		| Lunop of uop * expr
+		| Runop of expr * uop
+		| Assign of string * expr
+		| Call of string * expr list
+
+type stmt = Block of stmt list
+		| If of expr * stmt * stmt
+		| Elseif of expr * stmt * stmt
+		| For of expr * expr * expr * stmt
+		| While of expr * stmt
+		| Return of expr
+		| Expr of expr * ending
+
+type func_decl = {
+	typ		: typ;
+	fname	: string;
+	formals	: bind list;
+	locals	: bind list;
+	body	: stmt list;
+}
+
+type program = bind list * func_decl list;
