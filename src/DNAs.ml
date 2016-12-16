5d63db34a1a84b6f399e393c145758d6d3ceb618
fuentesori <fuentesori@gmail.com>
2016-12-16 18:13:13 -0500

    added semant to run file

--- src/DNAs.ml
+++ src/DNAs.ml
@@ -4,13 +4,14 @@ type action = Ast | Llvm | Compile
 
 let _ =
    let action = if Array.length Sys.argv > 1 then
-    List.assoc Sys.argv.(1) [ ("-a", Ast); 
-            ("-l", Llvm);  
+    List.assoc Sys.argv.(1) [ ("-a", Ast);
+            ("-l", Llvm);
             ("-c", Compile) ]
   else Compile in
 
   let lexbuf = Lexing.from_channel stdin in
   let ast = Parser.program Scanner.token lexbuf in
+  Semant.check ast;
   match action with
     Ast-> print_string (Ast.string_of_program ast)
   | Llvm-> print_string (Llvm.string_of_llmodule (Codegen.translate ast))
